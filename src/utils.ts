09d0303583d7506313aeaf12fa622d33228fe26a
Kylian Bardini <kylianb1@icloud.com>
2023-03-17 02:53:15 +0100

    🔧 update: update config

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,18 @@
 import { getEntryBySlug } from 'astro:content';
-
+/**
+ * 
+ * @param lang defines the language of the content
+ * @returns content of the footer
+ */
 export async function getFooter(lang: string) {
     let collection = await getEntryBySlug('footer', lang);
     return collection?.data;
 }
-
+/**
+ * 
+ * @param lang defines the language of the content
+ * @returns content of the nav
+ */
 export async function getNav(lang: string) {
     let collection = await getEntryBySlug('nav', lang);
     return collection?.data;
