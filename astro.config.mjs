63067d42fc3089524a9a27c2e0799c4fda2f0278
Kylian Bardini <kylianb1@icloud.com>
2023-03-17 15:47:04 +0100

    ➕ dep: add @astro/vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx(), sitemap()]
+  integrations: [tailwind(), react(), mdx(), sitemap()],
+  output: "server",
+  adapter: vercel({ analytics: true }),
 });
