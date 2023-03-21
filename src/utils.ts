b6cb0078ea8552ff1bbd16f5e151a7b1fdebdcac
Kylian Bardini <kylianb1@icloud.com>
2023-03-21 17:09:38 +0100

    ⚡️ feat: add ignore elements on getArticles

--- src/utils.ts
+++ src/utils.ts
@@ -21,9 +21,12 @@ export async function getArticles<T extends 'blog' | 'stack' | 'projects'>(
   collection: T,
   lang: 'en' | 'fr',
   limit?: number,
+  ignore?: string,
 ) {
   let collectionEntries = await getCollection(collection),
     sortedArticlesByLang = collectionEntries.filter(entry => entry.id.startsWith(lang))
+  if (ignore)
+    sortedArticlesByLang = sortedArticlesByLang.filter(entry => entry.slug != ignore)
   if (limit) return sortedArticlesByLang.slice(0, limit)
   else
     return sortedArticlesByLang.sort((a, b) => {
@@ -35,9 +38,12 @@ export async function getArticlesByPublishDate<T extends 'blog' | 'stack' | 'pro
   collection: T,
   lang: 'en' | 'fr',
   limit?: number,
+  ignore?: string,
 ) {
   let collectionEntries = await getCollection(collection),
     sortedArticlesByLang = collectionEntries.filter(entry => entry.id.startsWith(lang))
+  if (ignore)
+    sortedArticlesByLang = sortedArticlesByLang.filter(entry => entry.slug != ignore)
   if (limit) return sortedArticlesByLang.slice(0, limit)
   else
     return sortedArticlesByLang.sort((a, b) => {
