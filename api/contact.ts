a4877f40f995cc49f9cc38da1ba22b68b1a94376
Kylian Bardini <kylianb1@icloud.com>
2023-04-11 18:05:04 +0200

    ➕ add vercel dependency

---
+++ api/contact.ts
@@ -0,0 +1,3 @@
+/* Middleware to handle contact form with vercel serverless functions using astro framework
+ Send email to "kylianb1@icloud.com"
+*/
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,28 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import react from '@astrojs/react'
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import astroI18next from 'astro-i18next'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import astroI18next from 'astro-i18next';
+import image from '@astrojs/image';
 
-import image from '@astrojs/image'
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kbrdn.dev',
-  integrations: [
-    tailwind(),
-    react(),
-    mdx(),
-    sitemap({
-      i18n: {
-        defaultLocale: 'fr',
-        locales: {
-          en: 'en-US',
-          fr: 'fr-FR',
-        },
-      },
-    }),
-    astroI18next(),
-    image(),
-  ],
-})
+  integrations: [tailwind(), react(), mdx(), sitemap({
+    i18n: {
+      defaultLocale: 'fr',
+      locales: {
+        en: 'en-US',
+        fr: 'fr-FR'
+      }
+    }
+  }), astroI18next(), image()],
+  output: "server",
+  adapter: vercel({
+    analytics: true,
+    includeFiles: ['./api/*.ts']
+  })
+});
